8e1821302cf74a9da0b6dbd4030ccb190ca5e73c
_Damw <82566102+damw-glitch@users.noreply.github.com>
2024-12-23 02:26:18 -0500

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,22 @@
-const path = require('path');
+import { resolve } from 'path';
 
-module.exports = {
-  entry: './app.js',
-  output: {
+export const entry = './app.js';
+export const output = {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
-      }
-    ]
-  },
-  mode: 'development'
+    path: resolve(__dirname, 'dist')
 };
+export const module = {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+        }
+    ]
+};
+export const mode = 'development';
