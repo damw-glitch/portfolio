f5b67f06eb3787b9f69a5d42da0a69087f36a8f9
_Damw <82566102+damw-glitch@users.noreply.github.com>
2024-12-24 02:38:51 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 
 console.log('Script is running');
 
+const canvas = document.querySelector('.webgl');
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
