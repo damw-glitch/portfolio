07965a2083014143547691765d8afa11604b38dc
_Damw <82566102+damw-glitch@users.noreply.github.com>
2024-12-24 02:07:25 -0500

    Delete copy-html.js

--- copy-html.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const src = path.join(__dirname, 'index.html');
-const dest = path.join(__dirname, 'dist', 'index.html');
-
-fs.copyFileSync(src, dest);
-console.log('index.html copied to dist directory');
